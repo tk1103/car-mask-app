6c3c4d12fda13b4112c26033751b6bd0cbc8849f
國安智人 <73391909+tk1103@users.noreply.github.com>
2026-02-16 12:32:40 +0900

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: {
-    // プロジェクトルートを絶対パスで固定
-    root: '/Users/heidegger/AutoCamera',
-  },
-};
-
-export default nextConfig;
