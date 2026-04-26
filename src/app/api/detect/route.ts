8da733dfcf0d2de2e32b8c9aaedcabb341c2cbea
國安智人 <73391909+tk1103@users.noreply.github.com>
2026-04-26 12:57:55 +0900

    Update route.ts

--- src/app/api/detect/route.ts
+++ src/app/api/detect/route.ts
@@ -1,1 +1,7 @@
-export { runtime, maxDuration, GET, POST } from '../detect-plate/route';
+import { GET as detectPlateGET, POST as detectPlatePOST } from '../detect-plate/route';
+
+export const runtime = 'nodejs';
+export const maxDuration = 60;
+
+export const GET = detectPlateGET;
+export const POST = detectPlatePOST;
